9ea96a058610fabe20c69ef879208da73d4ec561
Neha <147375758+0406neha@users.noreply.github.com>
2023-12-08 13:57:21 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,55 @@
+console.log('hello');
+// alert('yooo');
+
+//variables
+var a = 'Banana';
+console.log(a);
+
+var anyNumber = 16;
+console.log(anyNumber);
+
+// var myName = prompt('What is your name?');
+
+// document.getElementById('paraGraph').innerHTML = myName;
+
+//Numbers in javascript
+var num1 = 10;
+// num1 = num1 + 1; //incrementing
+num1++; //incrementing
+
+num1--; //decrementing
+
+console.log(num1);
+
+/* Functions
+1. Create a function
+2. Call a function
+*/
+
+// Create
+function fun() {
+  console.log('this is a function');
+}
+
+// Call
+fun();
+
+// Return Function
+
+function greeting(yourName) {
+  var result = 'Hello' + ' ' + yourName; //String Concatenation
+  console.log(result);
+}
+var aName = prompt('What is your name?');
+greeting(aName);
+
+// Function arguements
+
+function aNumber(num1, num2) {
+  var result = num1 + num2;
+  console.log(result);
+}
+
+// aNumber(15, 14);
+// aNumber('10', 10);
+aNumber('10', '10');
