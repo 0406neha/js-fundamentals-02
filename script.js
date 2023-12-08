e06d00c687fc626ef086542776a2f3a2fd107f0f
Neha <147375758+0406neha@users.noreply.github.com>
2023-12-08 13:57:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ logger();
 logger();
 logger();
 logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log;
