26d638e4d46d346926e97d02400b15d68d68e6f1
Neha <147375758+0406neha@users.noreply.github.com>
2023-12-19 14:00:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-'use strict';
+// 'use strict';
 
-function logger() {
-  console.log('My Name is Neha');
+// function logger() {
+//   console.log('My Name is Neha');
+// }
+
+// // Calling / Running / Invoking function
+// logger();
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log;
+
+// // functions
+
+// function printMe() {
+//   console.log('printing...');
+// }
+
+// printMe();
+
+// //Parameter : when u envokes the function
+
+// //Arguement : when u calls the function
+
+// function printThis(NEHA) {
+//   console.log(NEHA);
+// }
+
+// printThis('ISHA');
+
+// // const count =  100;  (Expression)
+
+// const printMeAgain = function () {
+//   console.log('print');
+// };
+
+// printMeAgain();
+
+// const printMeAgainParameter = function (x, y) {
+//   console.log(x, y);
+// };
+
+// printMeAgainParameter(50, 20);
+
+function sum(a, b) {
+  let ret = a + b;
+  return ret;
 }
 
-// Calling / Running / Invoking function
-logger();
-logger();
-logger();
-logger();
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log;
+sum(2, 3);
